58879b25cd8b65df2871f5f1d9e56cde05bcb240
mmacura9 <marko.macura9@gmail.com>
2024-11-16 05:05:01 +0700

    Add backend

--- api/src/server/index.ts
+++ api/src/server/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import http from "http";
+
+export async function startServer() {
+    const serverImport = await import("./server");
+    const server = await serverImport.default();
+    const Server: http.Server = http.createServer(server);
+
+    Server.listen(server.get("port"), () => {
+        console.log(`listening on port:${server.get("port")}`);
+    });
+}
+
+startServer();
