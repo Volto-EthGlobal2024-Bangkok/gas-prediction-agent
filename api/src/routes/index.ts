33caac928b8a7eb6c33a5a0383b24153a6c07380
mmacura9 <marko.macura9@gmail.com>
2024-11-17 03:23:54 +0700

    Add cors for localhost

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,6 +3,8 @@ import express, {NextFunction, Request, Response} from "express";
 import PredictionRouter from "./PredictionRouter";
 import GasRouter from "./GasRouter";
 
+import cors from "cors";
+
 export function init(app: express.Application): void {
     const router: express.Router = express.Router();
     router.get(
@@ -22,4 +24,20 @@ export function init(app: express.Application): void {
     router.use("/api/v1/gas", GasRouter);
 
     app.use(router);
+
+    const corsOptions = {
+        origin: (requestOrigin, callback) => {
+            // Allow requests with no origin (like mobile apps or curl requests)
+            if (!requestOrigin) return callback(null, true);
+
+            if (requestOrigin.match(/^http:\/\/localhost:\d+$/)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    };
+    router.use(cors(corsOptions));
 }
