bbc462e13fcb9a7cab4b8bd7eaed25b4db096318
mmacura9 <marko.macura9@gmail.com>
2024-11-17 02:47:19 +0700

    Update model loading

--- api/src/services/prediction/index.ts
+++ api/src/services/prediction/index.ts
@@ -8,8 +8,8 @@ export async function predict(
 ) {
     try {
         const days = parseInt(req.params.days);
-        const faqs = await PredictionService.predict(days);
-        res.status(200).json(faqs);
+        const prediction = await PredictionService.predict(days);
+        res.status(200).json(prediction);
     } catch (error) {
         console.log(
             `Error found in ${__filename} - getFAQs method: ${error.message}`,
