7fbf17c4fe7576d0d3f3a2d1b265d20fcf6d34d5
mmacura9 <marko.macura9@gmail.com>
2024-11-16 15:45:21 +0700

    Add catches for model

---
+++ ai-model/src/routes/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, APIRouter
+import uvicorn
+import sys
+sys.path.append('./src/models')
+from inference import inference
+
+app = FastAPI()
+
+@app.get("/health")
+async def root():
+    return {"message": "Hello World"}
+
+root_router = APIRouter(prefix="/api/v1", tags=["predict"])
+
+@root_router.get("/predict/{days}", tags=["predict"])
+async def read_users(days: int):
+    days = int(days)
+    print(days)
+    if days > 5:
+        raise HTTPException(status_code=404, detail="Too many days")
+
+    return {"prediction": inference(days)}
+
+app.include_router(root_router)
---
+++ ai-model/src/routes/predict.py
@@ -0,0 +1,10 @@
+# from fastapi import APIRouter, FastAPI
+# import app from main
+#
+# router = APIRouter(prefix="/api/v1/predict", tags=["predict"])
+#
+# @router.get("/", tags=["predict"])
+# async def read_users():
+#     return [{"username": "Rick"}, {"username": "Morty"}]
+#
+# app.include_router(router)
